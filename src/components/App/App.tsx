3ff4ef6058292d8a11887a7ac36a60362d74905b
Nikita <nikita-prokuratov@mail.ru>
2023-01-15 18:18:31 +0300

    init

---
+++ src/components/App/App.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import logo from './logo.svg';
+import styles from './App.module.scss';
+
+export default function App() {
+  return (
+    <div className={styles.app}>
+    </div>
+  );
+}
